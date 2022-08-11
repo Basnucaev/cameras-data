fb723e10fd3ec317580d24e949c1aeb12f3c8a03
Basnucaev <96333647+Basnucaev@users.noreply.github.com>
2022-08-11 13:12:07 +0300

    Init commit

---
+++ src/test/java/com/basnukaev/camerasdata/CamerasDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basnukaev.camerasdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamerasDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
